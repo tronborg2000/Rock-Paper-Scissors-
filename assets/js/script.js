2681674cc88efda135f2808fdec96809c8c9abfa
tronborg2000 <colinweldon1980@gmail.com>
2022-11-06 11:20:47 +0000

    add dom const add eventlisteners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
-const score = document.querySelector('.result');
-const buttons = document.querySelectorAll('button');
-const winnerScores = [0,0];
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const message = document.getElementById("messages")
+const choices = ["rock", "paper", "scissors", "lizard", "spock"]
 
 //add event listeners to buttons
-for ( let i = 0 ; i < buttons.length ; i++){
-    buttons[i].addEventListener('click', playGame);
+for ( let button of buttons) {
+    buttons.addEventListener("click", function() {
+        let playerChoice= this.getAttribute("data-choice");
+        playgame(playerChoice);
+    });
 }
 
 
